c1c09eea853ef442d943e9b56ebf74ea42fbe29f
Sergey <34822195+SNikolaevsky@users.noreply.github.com>
2018-06-25 02:24:47 +0300

    Create Breaker.py

---
+++ Breaker.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+#var = 1
+#while var == 1 :
+#    time.sleep(10)
+#    webbrowser.open("https://www.youtube.com/watch?v=H2r9KBgF6aI",new=2)
+
+count = 0
+while count < 5:
+   time.sleep(10)
+   webbrowser.open("https://www.youtube.com/watch?v=H2r9KBgF6aI",new=2)
+   count = count + 1
