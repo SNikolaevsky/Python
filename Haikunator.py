45211b769ae15434ce877223616d7366d3ac21c0
Sergey <34822195+SNikolaevsky@users.noreply.github.com>
2018-12-14 17:37:18 +0300

    Add files via upload

---
+++ Haikunator.py
@@ -0,0 +1,16 @@
+from haikunator import Haikunator
+
+haikunator = Haikunator()
+
+#print (haikunator.haikunate(token_length=0))
+
+i = 1
+while i < 30:
+    print(haikunator.haikunate(token_length=0))
+    i=i+1
+
+
+
+#for (i < 100 (i++))
+#    try:
+#print (haikunator.haikunate(token_length=0))
